791f4c557ece850acdae3cc582e9ac68b302facd
Jonnathan Cruz <jonnathan.cruz@aec.com.br>
2023-08-25 14:13:28 -0300

    subindo insert usuarios

--- index.js
+++ index.js
@@ -44,12 +44,23 @@ app.get('/usuarios', (req, res) => {
 
 import cors from 'cors';
 
-app.use(cors({origin: 'http://localhost:8101'}));
+app.use(cors({origin: 'http://localhost:8100'}));
 
 app.post('/usuarios', async (req, res) => {
+  admin.firestore()
+  .collection('usuarios').add({
+    cargo: req.body.cargo,
+    descricaoCargo: req.body.descricaoCargo,
+    email: req.body.email,
+    empresa: req.body.empresa,
+    idUsuario: req.body.idUsuario,
+    nome: req.body.nome,
+    senha: req.body.senha
+  })
+
   const data = req.body;
  
-  res.status(200).send({ msg: "Usuario criado com sucesso"})
+  res.status(200).send({ msg: "Usuario criado com sucesso", response : res.data})
 
 })
 
