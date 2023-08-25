c43bb0f837f711a657d9c6b918723a0d9a57f5dc
Jonnathan Cruz <jonnathan.cruz@aec.com.br>
2023-08-24 22:58:57 -0300

    instalando dependencia cors

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 // var http = require('http');
 // let app = express();
 
-// var cors = require('cors');
 
 // // use it before all route definitions
-// app.use(cors({origin: 'http://localhost:8100'}));
 
 // app.post("/adicionarUsuario", ( req,res) => {
 //     res.setHeader('Content-Type', 'application/json');
@@ -42,6 +40,17 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 
     })
+})
+
+import cors from 'cors';
+
+app.use(cors({origin: 'http://localhost:8101'}));
+
+app.post('/usuarios', async (req, res) => {
+  const data = req.body;
+ 
+  res.status(200).send({ msg: "Usuario criado com sucesso"})
+
 })
 
 
